15293630ba68945624acf0eb5e780fd84d9785b4
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2024-03-18 15:16:05 -0300

    Switch Icons Sum-Moon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import Switch from "react-switch";
 import { HeaderContainer, Secction2, Secction1 } from "./Header.style";
 import { DefaultTheme, ThemeContext } from "styled-components";
+import NightlightRoundIcon from "@mui/icons-material/NightlightRound";
+import Brightness5Icon from "@mui/icons-material/Brightness5";
 
 import { shade } from "polished";
 
@@ -10,10 +12,10 @@ interface Props {
 }
 
 export const Header: React.FC<Props> = ({ toggleTheme }) => {
-  const theme = useContext<DefaultTheme|undefined>(ThemeContext);
+  const theme = useContext<DefaultTheme | undefined>(ThemeContext);
 
   if (!theme) {
-    return undefined;
+    return null;
   }
 
   const { colors, title } = theme;
@@ -22,20 +24,7 @@ export const Header: React.FC<Props> = ({ toggleTheme }) => {
     <HeaderContainer>
       <Secction1>
         {" "}
-        <p>logo</p>{" "}
-        <div>
-          <Switch
-            onChange={toggleTheme}
-            checked={title == "dark"}
-            checkedIcon={false}
-            uncheckedIcon={true}
-            height={10}
-            width={40}
-            handleDiameter={20}
-            offColor={shade(0.1, colors.primary)}
-            onColor={shade(0.2, colors.secondary)}
-          />
-        </div>
+        <p>Logo</p>
       </Secction1>
 
       <Secction2>
@@ -50,6 +39,20 @@ export const Header: React.FC<Props> = ({ toggleTheme }) => {
         <div>
           <p>Img Linkedin</p>
           <p>Image Github</p>
+        </div>
+        <div>
+          {title == "dark" ? <NightlightRoundIcon /> : <Brightness5Icon />}{" "}
+          <Switch
+            onChange={toggleTheme}
+            checked={title == "dark"}
+            checkedIcon={false}
+            uncheckedIcon={true}
+            height={10}
+            width={40}
+            handleDiameter={20}
+            offColor={shade(0.1, colors.primary)}
+            onColor={shade(0.2, colors.secondary)}
+          />
         </div>
       </Secction2>
     </HeaderContainer>
