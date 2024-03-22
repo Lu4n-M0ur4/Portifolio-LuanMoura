779d55ea3fccf572abd481ada1c8014348fc0214
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2024-03-21 23:03:50 -0300

    Arquivos reorganizados

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./normalize.css";
+import "./styles/normalize.css"
 import { GlobalProvider } from "./context/GlobalContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
